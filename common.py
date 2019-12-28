2b414028373ea6c15e6e0e4fb20cfb265b47789e
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-27 18:51:52 -0600

    Update common.py
    this will prevent me from having to manually copy and past from single select field into the linked record field. Doing a switch/case method in the common module makes the main module from being unreadable.

--- common.py
+++ common.py
@@ -46,11 +46,36 @@ cloudinary.config(cloud_name='bfeldman89',
 muh_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
 
+my_funcs = {'acdc_scraper': 'recZdPJHytCXB3v7C',
+            'ccdc_scraper': 'recDAFRGM4gkPhHEO',
+            'hcdc_scraper': 'rec3CEVirsVhbogFp',
+            'jcadc_scraper': 'recVgj1nGL6H79bNZ',
+            'jcj_scraper': 'recQIKW9Ky2K5s9sS',
+            'kcdc_scraper': 'recVkA9ZjjDC7WHMl',
+            'lcdc_scraper': 'recqvYm2sWOVXVFwE',
+            'mcdc_scraper': 'rec2kqwh9Nj8jyeqW',
+            'prcdf_scraper': 'recsbEUfUO9WjXi8I',
+            'tcdc_scraper': 'rec19KIgPSPYX8dyG',
+            'fix_charges_to_by_lines': 'rechl6T1MRB4EK9NF',
+            'get_all_intake_deets': 'recz3G21RAFoebeqe',
+            'get_charges_from_recent_text': 'rec1QQGOdKrliYy52',
+            'get_full_text': 'recszZqgqyMZbrZWA',
+            'get_pixelated_mug': 'rece3aWHAWx2dAv09',
+            'parse_charge_1': 'recndlqiBduVHEekv',
+            'remove_weird_character': 'recDlfvTxn2YQyJ7V',
+            'retry_getting_mugshot': 'rechPDYn5Koib8fjq',
+            'update_summary': 'reczRWJZM7KIz1LRd',
+            'get_dor_if_possible': 'recFMBytEdFfWEghZ',
+            'pdf_to_dc': 'recqUaIfPApOsw6SB',
+            'web_to_pdf': 'recFWboNnGFfVBnnS'}
+
+
 def wrap_from_module(module):
     def wrap_it_up(t0, new=None, total=None, function=None):
         this_dict = {
             'module': module,
             'function': function,
+            '_function': my_funcs[function],
             'duration': round(time.time() - t0, 2),
             'total': total,
             'new': new
