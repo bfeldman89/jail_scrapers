4898f58afb526e96720fd6fa7482d509877aa220
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-03-28 04:30:48 -0500

    Update snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -18,15 +18,16 @@ county_jails = [('Madison', 'mcdc'),
 
 def pop_otd(day, county, jail, quiet=True):
     record = airtab_daily.match('date_str', day)
-    # day_before = date.fromisoformat(day) - timedelta(1)
-    # day_after = date.fromisoformat(day) + timedelta(1)
     day_before = datetime.strptime(day, '%Y-%m-%d') - timedelta(1)
     day_after = datetime.strptime(day, '%Y-%m-%d') + timedelta(1)
     pop_formula = f"AND(IS_BEFORE(DOI, '{day_after.date()}'), IS_AFTER(last_verified, '{day_before.date()}'), jail='{jail}')"
     records = airtab_intakes.get_all(fields='jail', formula=pop_formula)
+    # other_records = airtab_archive_intakes.get_all(fields='jail', formula=pop_formula)
+    # this_dict = {f"{county} pop": len(records) + len(other_records)}
     this_dict = {f"{county} pop": len(records)}
     airtab_daily.update(record['id'], this_dict)
     if not quiet:
+        # print(this_dict, f"active, {len(records)}; archive, {len(other_records)}")
         print(this_dict)
 
 
@@ -34,9 +35,12 @@ def admits_otd(day, county, jail, quiet=True):
     record = airtab_daily.match('date_str', day)
     admits_formula = f"AND(DATETIME_FORMAT(DOI, 'YYYY-MM-DD')='{day}', jail='{jail}')"
     records = airtab_intakes.get_all(fields='jail', formula=admits_formula)
+    # other_records = airtab_archive_intakes.get_all(fields='jail', formula=admits_formula)
+    # this_dict = {f"{county} admits": len(records) + len(other_records)}
     this_dict = {f"{county} admits": len(records)}
     airtab_daily.update(record['id'], this_dict)
     if not quiet:
+        # print(this_dict, f"active, {len(records)}; archive, {len(other_records)}")
         print(this_dict)
 
 
