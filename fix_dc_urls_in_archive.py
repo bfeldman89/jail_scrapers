46183df4c865169c4506ceb21653c7bbd6d9f90c
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2020-11-25 19:57:05 -0600

    Update fix_dc_urls_in_archive.py

--- fix_dc_urls_in_archive.py
+++ fix_dc_urls_in_archive.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import sys
 import time
-from common import airtab_archive_intakes as airtab2
+from common import airtab_archive_intakes as airtab
 from common import dc
 
-def update_dc_fields2(how_many, nap):
-    records = airtab2.get_all(view='need dc urls updated', fields='dc_id', max_records=how_many)
+def update_dc_fields(how_many, nap):
+    records = airtab.get_all(view='need dc urls updated', fields='dc_id', max_records=how_many)
     print(len(records), ' archived records need updated documentcloud URLs.')
     for record in records:
         this_dict = {}
@@ -14,15 +14,13 @@ def update_dc_fields2(how_many, nap):
         this_dict["PDF"] = obj.pdf_url
         this_dict["dc_canonical_url"] = obj.canonical_url
         this_dict["dc_resources_page_image"] = obj.normal_image_url
-        airtab2.update(record['id'], this_dict)
+        airtab.update(record['id'], this_dict)
         time.sleep(nap)
 
 def main():
     how_many = int(sys.argv[1])
     nap = int(sys.argv[2]) / 10
-    print(f'about to fix {how_many} records... ')
-    print(f'taking a {nap}-second nap between records...')
-    update_dc_fields2(how_many, nap)
+    update_dc_fields(how_many, nap)
 
 
 if __name__ == "__main__":
